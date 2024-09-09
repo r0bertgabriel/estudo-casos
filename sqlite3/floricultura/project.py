fdc8d03ab0deddcceeaa4a6d33bdcba71d7603ed
Robert Gabriel <151944337+br4b1n@users.noreply.github.com>
2024-09-09 17:05:52 -0300

    Update project.py

--- sqlite3/floricultura/project.py
+++ sqlite3/floricultura/project.py
@@ -13,12 +13,14 @@ Rua VARCHAR(40),
 Numero VarChar(5),
 Bairro VARCHAR(25)
 );
+
 """
+
 sql_produtos = """
 CREATE TABLE IF NOT EXISTS Produto (
 ID_Produto INTEGER PRIMARY KEY AUTOINCREMENT,
 Nome_Produto VARCHAR(30) NOT NULL,
-Tipo_Prroduto VARCHAR(25) NOT NULL,
+Tipo_Produto VARCHAR(25) NOT NULL,
 Preco DECIMAL(10,2) NOT NULL,
 Qtde_Estoque SMALLINT NOT NULL
 );
@@ -64,4 +66,82 @@ except con.DatabaseError as erro:
 finally:
     if conexao:
         conexao.close()
+# %% INSERINDO dados na tabela Cliente
+insert_cliente = """
+INSERT INTO Cliente (RG, Nome_Cliente, Sobrenome_CLiente, Telefone, Rua, Numero, Bairro)
+VALUES
+('123456789', 'João', 'Silva', '123456789', 'Rua 1', '123', 'Centro'),
+('987654321', 'Maria', 'Santos', '987654321', 'Rua 2', '456', 'Bela Vista')
+
+"""
+try:
+    conexao = con.connect('floricultura.db')
+    cursor = conexao.cursor()
+
+    cursor.execute(insert_cliente)
+
+    conexao.commit()
+except con.DatabaseError as erro:
+    print("Erro no Banco de dados", erro)
+else:
+    #verificar se dados foram cadastrados
+    res = cursor.execute("SELECT * FROM Cliente;")
+    print(res.fetchall())
+finally:
+    if conexao:
+        conexao.close()
+
+# %% inserir dados na tabela Produtos
+insert_produto = """
+INSERT INTO Produto (Nome_Produto, Tipo_Produto, Preco, Qtde_Estoque)
+VALUES
+('Rosa', 'Flor', 10.00, 100),
+('Girassol', 'Flor', 5.00, 50),
+('Violeta', 'Flor', 3.00, 200),
+('Vaso', 'Acessorio', 20.00, 30),
+('Orquidea', 'Flor', 15.00, 80)
+
+"""
+try:
+    conexao = con.connect('floricultura.db')
+    cursor = conexao.cursor()
+
+    conexao.execute(insert_produto)
+    conexao.commit()
+except con.DatabaseError as erro:
+    print("Erro no Banco de dados", erro)
+else:
+    # verificar se dados foram cadastrados
+    res = cursor.execute("SELECT * FROM Produto;")
+    print(res.fetchall())
+finally:
+    if conexao:
+        conexao.close()
+
+# %% inserir dados na tabela Vendas
+insert_venda = """
+INSERT INTO Venda(Nota_Fiscal, ID_Cliente, Data_Compra, ID_Produto, Quantidade)
+VALUES
+(123, 1, '2021-09-01', 1, 10),
+(124, 2, '2021-09-02', 2, 5),
+(125, 1, '2021-09-03', 3, 20),
+(126, 2, '2021-09-04', 4, 2),
+(127, 1, '2021-09-05', 5, 10)
+"""
+
+try:
+    conexao = con.connect('floricultura.db')
+    cursor = conexao.cursor()
+
+    cursor.execute(insert_venda)
+    conexao.commit()
+except con.DatabaseError as erro:
+    print("Erro no Banco de dados", erro)
+else: # verificar se dados foram cadastrados
+    res = cursor.execute("SELECT * FROM Venda;")
+    print(res.fetchall())
+finally:
+    if conexao:
+        conexao.close()
+
 # %%
